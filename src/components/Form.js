ae05230c5f37c105d840271b5cc8a7db126ddb64
Tatiana Smolin <tatismolin@gmail.com>
2020-03-11 12:54:53 -0600

    Adds numbers to weords function and fixes the zero issue.

--- src/components/Form.js
+++ src/components/Form.js
@@ -26,9 +26,61 @@ class Form extends Component{
         const {value} = this.state;
         event.preventDefault();
         this.resetForm();
-        // this.setState({
-        //     newValue: value
-        // });
+        this.setState({
+            newValue: this.toWords(value)
+        });
+    };
+
+    toWords = (number) => {
+
+        const groupA = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
+        const groupB = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
+        const groupC = "hundred";
+        const groupD = "thousand million billion trillion".split(" ");
+
+        let givenNumber = number;
+        let result = [];
+        let comma = 0;
+
+        while(givenNumber > 0){
+
+            let words = [];
+            let n = givenNumber % 1000;
+            givenNumber = Math.floor(givenNumber / 1000);
+
+            if(n > 99){
+                words.push(groupA[Math.floor(n / 100)]);
+                words.push(groupC);
+                n %= 100
+            };
+
+            if(n > 19){
+                words.push(groupB[Math.floor(n / 10) - 2]);
+                n %= 10;
+            };
+
+            if(n > 0){
+                words.push(groupA[n]);
+            };
+
+            if(words.length > 0){
+                if(comma > 0){
+                    words.push(groupD[comma - 1]);
+                };
+                result.push(words.join(" "));
+            };
+
+            comma++;
+
+        };
+
+        if(result.length > 0){
+            result = result.reverse();
+            return result.join(" ");
+        }else{
+            return "zero"
+        };
+
     };
 
     render(){
